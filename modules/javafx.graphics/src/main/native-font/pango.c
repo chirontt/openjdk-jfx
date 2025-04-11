4b9867ff21e8bc89bcf77c71cae19beb9322d9b4
Tue Ton <49886739+chirontt@users.noreply.github.com>
2025-04-11 16:35:59 -0400

    port to FreeBSD
    by work from "didier" member:
    https://forums.freebsd.org/threads/openjfx-17-and-scenebuilder-17-work-on-freebsd-13.83149/
    with some minor fixes for FreeBSD 14.1
    
    Also, build for Windows on Arm64 is supported, using the MSYS2
    environment for Arm64 and MS Visual Studio 2022.

--- modules/javafx.graphics/src/main/native-font/pango.c
+++ modules/javafx.graphics/src/main/native-font/pango.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
  * questions.
  */
 
-#if defined __linux__
+#if defined __FreeBSD__ || defined __linux__
 #if defined _ENABLE_PANGO
 
 #include <jni.h>
